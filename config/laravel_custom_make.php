a39fb210a5f3f034d85319a1c11b1206c0833891
Usman Zahid <developerusman@yahoo.com>
2025-04-20 20:40:13 +0500

    Improved the current structure

--- config/laravel_custom_make.php
+++ config/laravel_custom_make.php
@@ -1,8 +1,24 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Make Types
+    |--------------------------------------------------------------------------
+    |
+    | Define your custom class types here. Each type should include:
+    | - 'path': The directory where the generated class should be placed.
+    | - 'stub': The path to the stub file used as a template for generation.
+    |
+    | You can add as many types as needed. The `make:custom {type} {name}`
+    | command will use this configuration to create the appropriate class.
+    |
+    */
+
     'service' => [
         'path' => app_path('Services'),
         'stub' => base_path('stubs/service.stub'),
     ],
+
 ];
