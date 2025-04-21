b8e0ad0735846d8d98c87b230a5d6c3fab6f48ea
Usman Zahid <developerusman@yahoo.com>
2025-04-21 22:20:33 +0500

    Fixed namespace bug

--- src/CustomMakeCommand.php
+++ src/CustomMakeCommand.php
@@ -75,7 +75,7 @@ class CustomMakeCommand extends Command {
     protected function getClassName(string $name): string {
         return class_basename($name);
     }
-
+ 
     protected function getNamespace(string $path): string {
         $relativePath = str_replace(base_path() . '/', '', $path);
         $namespace = str_replace(['/', '\\'], '\\', $relativePath);
